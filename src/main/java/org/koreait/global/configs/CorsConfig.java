79afa96be49f8e51924c88472fa057b5b0e4d313
SilenceTale <chomn55@gmail.com>
2025-01-14 10:59:54 +0900

    .

--- src/main/java/org/koreait/global/configs/CorsConfig.java
+++ src/main/java/org/koreait/global/configs/CorsConfig.java
@@ -1,13 +1,19 @@
 package org.koreait.global.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
+
+    @Value("${cors.allowed}")
+    private List<String> allowedOrigin;
 
     @Bean
     public CorsFilter corsFilter() {
@@ -17,6 +23,13 @@ public class CorsConfig {
 
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
+
+        if (allowedOrigin == null || allowedOrigin.isEmpty()) {
+            config.addAllowedOrigin("*");
+        } else {
+            config.setAllowedOrigins(allowedOrigin);
+            config.setAllowCredentials(true);
+        }
 
 
         source.registerCorsConfiguration("/**", config);
